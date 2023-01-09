eb5d5a3fc5e1807b871a768291e1a5f410ea37f8
David Tejero Ruiz <davidtejero1999@gmail.com>
2023-01-09 23:15:11 +0100

    Legend added to plots. Comments for different use cases

--- fsm_uav.py
+++ fsm_uav.py
@@ -6,15 +6,15 @@ import time
 
 MAP_SIZE = (10, 10, 10) # m x m x m
 
-N_OBSTACLES = 50
-N_UAVS = 3
+N_OBSTACLES = 10
+N_UAVS = 4
 
 K_GROUP = 1.5
 K_REPULSION = 1.0
 K_FRICTION = 0.25
 K_OBSTACLES = 1.0
 
-VEL_MAX = 1.0
+VEL_MAX = 1.0   # m/s
 MIN_FORCE = 0.1
 
 # Función generica para graficar puntos en 3D
@@ -110,8 +110,8 @@ def apply_forces(uavs, uavs_forces, dt):
 
 ##############################################
 uavs = generate_points(N_UAVS)
-# obstacles = generate_points(N_OBSTACLES)
-obstacles = np.array([])
+obstacles = generate_points(N_OBSTACLES)
+
 uavs_velocities = [np.zeros(3) for _ in range(len(uavs))]
 uavs_forces = [{} for i in range(len(uavs))]
 forces_keys = ['f_group','f_repulsion','f_friction','f_obstacles']
@@ -119,15 +119,27 @@ dt = 0.1
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-# cm = np.array([-MAP_SIZE[0]/2,0.0,0.0])
-# i = 0
+# Descomentar para añadir 4 obstáculos alrededor del origen
+# origin = np.array([0.0,0.0,0.0])
+# obstacles = []
+# obstacles.append(origin + np.array([0.0,1,0.0]))
+# obstacles.append(origin + np.array([0.0,-1,0.0]))
+# obstacles.append(origin + np.array([1,0.0,0.0]))
+# obstacles.append(origin + np.array([-1,0.0,0.0]))
+# obstacles = np.array(obstacles)
+
+i = 0
 while True:
-    tic = time.time()
     ax.clear()
+
+    # Sustituir por un punto de interés si se desea
     cm = center_of_mass(uavs)
-    # if i < 1000:
-    #     cm += np.array([0.01,0.00,0.00])
-    # i+=1
+
+    # Descomentar para mover el punto de interés 
+    # Nota: sacar fuera de bucle la inicialización de cm
+    # if i < 200:
+    #     cm += np.array([0.025,0.00,0.00])
+
     group_forces(uavs, cm)
     repulsion_forces(uavs, cm)
     friction_forces(uavs, uavs_velocities)
@@ -135,24 +147,32 @@ while True:
     resultant_forces(uavs_forces, forces_keys)
     plot_points(ax, uavs, color='b')
     plot_points(ax, np.array([cm]), color='r')
-    # plot_points(ax, obstacles, color='g')
+    plot_points(ax, obstacles, color='purple')
     plot_forces(ax, uavs, uavs_forces, 'f_repulsion', color='r')
     plot_forces(ax, uavs, uavs_forces, 'f_group', color='g')
-    # plot_forces(ax, uavs, uavs_forces, 'f_obstacles', color='y')
+    plot_forces(ax, uavs, uavs_forces, 'f_obstacles', color='purple')
     plot_forces(ax, uavs, uavs_forces, 'f_resultant', color='k')
     ax.set_xlim(-0.5*MAP_SIZE[0], 0.5*MAP_SIZE[0])
     ax.set_ylim(-0.5*MAP_SIZE[1], 0.5*MAP_SIZE[1])
     ax.set_zlim(-0.5*MAP_SIZE[2], 0.5*MAP_SIZE[2])
     apply_forces(uavs, uavs_forces, dt)
 
-    tac = time.time()
-    if tac - tic < dt:
-        time.sleep(dt - (tac - tic))
-    plt.pause(0.0001)
+    # Leyenda
+    ax.scatter([], [], c='b', label='UAVs')
+    ax.scatter([], [], c='r', label='TARGET Center of mass')
+    ax.scatter([], [], c='purple', label='Obstacles')
+    ax.quiver([], [], [], [], [], [], color='r', label='Repulsion force')
+    ax.quiver([], [], [], [], [], [], color='g', label='Group force')
+    ax.quiver([], [], [], [], [], [], color='purple', label='Obstacles force')
+    ax.quiver([], [], [], [], [], [], color='k', label='Resultant force')
+    ax.legend()
+
+    plt.pause(dt)
 
     if plt.get_fignums() == []:
         break
 
+    i+=1
 
 
     
